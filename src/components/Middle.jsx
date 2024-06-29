7dee4946bacd79261f1cea768fc4a4bcb8b2277a
simona <simonarumao@gmail.com>
2024-06-29 21:46:23 +0530

    next done

--- src/components/Middle.jsx
+++ src/components/Middle.jsx
@@ -5,30 +5,64 @@ import { FaPlane } from "react-icons/fa";
 export const Middle = () => {
   return (
       <>
-          <div className='flex justify-start ml-52'>
-            <div className=' rounded-lg bg-[#FFFFFF] p-2 flex w-[660px]'>
+    <div className='flex space-x-5 '>
+        <div className='flex justify-start ml-16 '>
+            <div className=' rounded-lg bg-[#FFFFFF] p-2 flex w-[660px] p-3'>
                 <div className='w-[60%] flex flex-col justify-between pt-3 pl-2'>
-                      <div className='pb-5'>
+                    <div className='pb-5'>
                         <p className='text-[16px] text-gray-500 font-semibold pb-1 '>Build by developers</p>
                         <p className='text-[20px] text-gray-700 font-bold pb-1'>Soft UI Dashboard</p>
                         <p className='text-[16px] text-[#78839A] w-[80%] '>From colors, cards, typography to complex elements, you will find the full documentation.</p>
                     </div>
                     <div>
-                        <button className='text-[16px] text-gray-500 font-semibold flex space-x-1 '>
+                        <button className='text-[16px] text-gray-500 font-semibold flex space-x-1 pb-2 '>
                             <div>
                                 Read More
                             </div>
-                            <div className='pt-[2px]'>
+                            <div className='pt-[6px]'>
                                 <FaArrowRight />
                             </div>
                         </button> 
                     </div>
                 </div>
-                <div className='w-[40%] p-[3px] h-[220px] rounded-lg bg-gradient-to-br from-[#21CDFD] to-[#1F61F1] flex justify-center items-center'>
+                <div className='w-[40%] p-3  h-[220px] rounded-lg bg-gradient-to-br from-[#21CDFD] to-[#1F61F1] flex justify-center items-center'>
                     <div> <img className='w-28'  src="https://www.learnmarketingtools.net/static/img/market-launch.png" alt="rocket" /></div>
                 </div>
             </div>
-            </div>
+              </div>
+              
+
+
+              {/* new comp */}
+  
+    <div className='relative rounded-lg p-3 bg-white w-[500px]'>
+  <div className='relative h-[220px]  '>
+    <img
+      className='h-[220px] w-[100%] object-cover rounded-lg '
+      src='https://i.pinimg.com/736x/77/71/10/777110032fa0c5faa81e28f7c21fc618.jpg'
+      alt=''
+    />
+    <div className='absolute top-0 left-0 w-full h-full flex flex-col justify-between p-3 bg-opacity-80 bg-[#1A2642] rounded-lg '>
+      <div>
+        <p className='text-white font-bold text-[20px] pt-2 pb-5'>Work with the rockets</p>
+        <p className='text-white font-medium'>
+          Wealth creation is an evolutionarily recent positive-sum game. It is all about who takes the opportunity first.
+        </p>
+      </div>
+      <div>
+        <button className='text-[16px] text-white font-semibold flex space-x-1'>
+          <div>Read More</div>
+          <div className='pt-[6px]'>
+            <FaArrowRight />
+          </div>
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
+    </div>
+          
+        
     </>
   )
 }
