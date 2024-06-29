2c1e9368bd3155b99b98ceb2ba129ed616215b3a
JonathanDabre <jonathandabre01@gmail.com>
2024-06-29 21:44:13 +0530

    added side panel bracket

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
 import { Growth } from './components/Growth';
 import { Middle } from './components/Middle';
+import SidePanel from './components/SidePanel';
 
 
 
 function App() {
   return (
     <>
-        <div className='bg-[#EFF0F1] pb-10'>
-        <Growth />
-        <Middle/>
+        <div className='bg-[#EFF0F1] pb-10 flex'>
+        <div className="">
+          <SidePanel/>
+        </div>
+        <div className="">        
+          <Growth />
+          <Middle/>
+        </div>
         </div>
     </>
   );
---
+++ src/components/SidePanel.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SidePanel = () => {
+  return (
+    <div className='w-[250px] bg-black h-[100vh]'>
+        hi
+    </div>
+  )
+}
+
+export default SidePanel
