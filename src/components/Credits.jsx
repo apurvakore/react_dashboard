9189bf7d1d578dab71e4ef7c324dfea2ddcd8cec
JonathanDabre <jonathandabre01@gmail.com>
2024-07-01 00:31:12 +0530

    final fixes

--- src/components/Credits.jsx
+++ src/components/Credits.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Credits = () => {
   return (
-    <div className='flex flex-col my-4 justify-center items-center'>
+    <div className='flex flex-col my-6 justify-center items-center'>
         <h2 className='text-[#67748E] font-semibold'>  © 2024, Crafted with <span>❤️</span> by Simona & Jonathan</h2>
         <p className="text-[#A6B0C2] text-sm font-semibold">I hope we keep tuning in for many such collaborations and for a Life Long Journey</p>
     </div>
