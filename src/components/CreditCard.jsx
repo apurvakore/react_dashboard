279b75704023ba7ef43e0be90a44a7f5c987c83a
simona <simonarumao@gmail.com>
2024-07-08 16:34:41 +0530

    hehe see my component

--- src/components/CreditCard.jsx
+++ src/components/CreditCard.jsx
@@ -6,12 +6,24 @@ export const CreditCard = () => {
         <div>
             <div className='flex'>
                 <div className='w-[50%]'></div>
-                <div className='w-[25%]'>
-                <div className='w-fit p-3 xl:p-[16px]  h-fit rounded-[6px] bg-gradient-to-br from-[#21CDFD] to-[#1F61F1]'>
-                        <div className='flex items-center justify-center'><BiSolidBank /></div>
-                    </div>
+                <div className='w-[25%]  bg-white flex flex-col justify-center items-center p-8  rounded-lg mr-6'>
+                    <div className='w-fit p-3 xl:p-[16px]  h-fit rounded-[9px] bg-gradient-to-br from-[#21CDFD] to-[#1F61F1] mb-4'>
+                            <div className='flex items-center justify-center'><BiSolidBank className='text-white text-[28px]' /></div>
+                        </div>
+                    <span className='text-[#344767] font-semibold mb-2'>Salary</span>
+                    <span className='text-xs text-[#9BA3B5] mb-4'>Belong Interactive</span>
+                 
+                    <span className='text-[#1E2634] font-bold border border-t-2 border-l-0 border-b-0 border-r-0 pt-4'>+$2000</span>
                 </div>
-                <div className='w-[25%]'></div>
+                <div className='w-[25%]  bg-white flex flex-col justify-center items-center p-8  rounded-lg'>
+                    <div className='w-fit p-3 xl:p-[16px]  h-fit rounded-[9px] bg-gradient-to-br from-[#21CDFD] to-[#1F61F1] mb-4'>
+                            <div className='flex items-center justify-center'><BiSolidBank className='text-white text-[28px]' /></div>
+                        </div>
+                    <span className='text-[#344767] font-semibold mb-2'>Salary</span>
+                    <span className='text-xs text-[#9BA3B5] mb-4'>Belong Interactive</span>
+                 
+                    <span className='text-[#1E2634] font-bold border border-t-2 border-l-0 border-b-0 border-r-0 pt-4'>+$2000</span>
+                </div>
             </div>
             <div></div>
         </div>
--- src/components/Invoices.jsx
+++ src/components/Invoices.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Invoices = () => {
   return (
     <div>
-        <div className='tab-name flex justify-between p-2'>
+        <div className='tab-name flex justify-between p-2 bg-white rounded-lg'>
             <div className="flex items-center font-semibold text-[#405270]">Invoice</div>
             <div className="">
                 <button className='px-4 py-2 text-sm font-semibold border border-[#51D1EE] rounded-xl '>
