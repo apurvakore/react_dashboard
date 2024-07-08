4f686557eac594404f5e64c8b7eb757e16c04d5a
simona <simonarumao@gmail.com>
2024-07-08 16:13:42 +0530

    jonathan said only billing tab and now credit card also

--- src/components/CreditCard.jsx
+++ src/components/CreditCard.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { BiSolidBank } from "react-icons/bi";
 
 export const CreditCard = () => {
-  return (
-    <div>CreditCard</div>
+    return (
+        <div>
+            <div className='flex'>
+                <div className='w-[50%]'></div>
+                <div className='w-[25%]'>
+                <div className='w-fit p-3 xl:p-[16px]  h-fit rounded-[6px] bg-gradient-to-br from-[#21CDFD] to-[#1F61F1]'>
+                        <div className='flex items-center justify-center'><BiSolidBank /></div>
+                    </div>
+                </div>
+                <div className='w-[25%]'></div>
+            </div>
+            <div></div>
+        </div>
   )
 }
