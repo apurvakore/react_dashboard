ee20f0fca4382c3fd56efef8859073ff35a036f6
simona <simonarumao@gmail.com>
2024-07-08 16:12:24 +0530

    joanthan said billingtab

--- src/components/BillingTab.jsx
+++ src/components/BillingTab.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { CreditCard } from './CreditCard'
 
+
+
 export const BillingTab = () => {
   return (
       <>
           <div className='flex'>
-              <div className='w-[65%] h-[400px] bg-blue-600 mr-6'>
+              <div className='w-[65%] h-[400px] border border-b-2 mr-6'>
                   <CreditCard/>
               </div>
 
