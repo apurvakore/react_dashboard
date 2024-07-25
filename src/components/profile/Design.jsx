145ed34edbc814bd07f4957105e38d9d099ccb60
simona <simonarumao@gmail.com>
2024-07-25 19:05:48 +0530

    design

--- src/components/profile/Design.jsx
+++ src/components/profile/Design.jsx
@@ -10,26 +10,53 @@ export const Design = () => {
                   <h6 className='text-[#344767] font-semibold'>Projects</h6>
                   <span className='text-[#727E96] text-sm font-medium'>Architects design houses</span>
                   <div className='flex space-x-5'>
-                      <div className='flex flex-col space-y-4 w-[25%]'>
+                      <div className='flex flex-col space-y-4 w-[25%] mb-4'>
                           <img src="https://themewagon.github.io/soft-ui-dashboard-react/static/media/home-decor-1.05e218fd495ccc65c99d.jpg" className='w-[280px] rounded-xl' alt="" />
                           <h6 className='text-[#343A63] text-[16px]'>Project #2</h6>
                           <h1 className='font-semibold text-lg text-[#344767]'>Modern</h1>
                           <p className='text-[#79849B] font-semibold text-sm'>As Uber works through a huge amount of internal management turmoil.</p>
+                          <div className='flex justify-between'>
+                            <button className='text-[#51D1EE] font-bold text-xs border py-2 px-9 rounded-lg border-[#51D1EE]'>VIEW PROJECT</button>
+                            <div class="flex -space-x-1 overflow-hidden">
+                            <img class="inline-block h-5 w-5 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
+                            <img class="inline-block h-5 w-5 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
+                            <img class="inline-block h-5 w-5 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80" alt=""/>
+                            <img class="inline-block h-5 w-5 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
+                            </div>  
+                          </div>
                       </div>
 
-                      <div className='flex flex-col space-y-4 w-[25%]'>
+                      <div className='flex flex-col space-y-4 w-[25%] mb-4'>
                           <img src="https://themewagon.github.io/soft-ui-dashboard-react/static/media/home-decor-2.b4e5397c9846645ba275.jpg" className='w-[280px] rounded-xl' alt="" />
                           <h6 className='text-[#343A63] text-[16px]'>Project #1</h6>
                           <h1 className='font-semibold text-lg text-[#344767]'>Scandinavian</h1>
                           <p className='text-[#79849B] font-semibold text-sm'>Music is something that every person has his or her own specific opinion about</p>
+                          <div className='flex justify-between'>
+                            <button className='text-[#51D1EE] font-bold text-xs border py-2 px-9 rounded-lg border-[#51D1EE]'>VIEW PROJECT</button>
+                            <div class="flex -space-x-1 overflow-hidden">
+                            <img class="inline-block h-5 w-5 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
+                            <img class="inline-block h-5 w-5 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
+                            <img class="inline-block h-5 w-5 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80" alt=""/>
+                            <img class="inline-block h-5 w-5 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
+                            </div>  
+                          </div>
                       </div>
 
 
-                      <div className='flex flex-col space-y-4 w-[25%]'>
+                      <div className='flex flex-col space-y-4 w-[25%] mb-4'>
                           <img src="https://themewagon.github.io/soft-ui-dashboard-react/static/media/home-decor-3.74d13fcbd3c631fc7908.jpg" className='w-[280px] rounded-xl' alt="" />
                           <h6 className='text-[#343A63] text-[16px]'>Project #3</h6>
                           <h1 className='font-semibold text-lg text-[#344767]'>Minimalist</h1>
                           <p className='text-[#79849B] font-semibold text-sm'>Different people have different taste, and various types of music.</p>
+                          <div className='flex justify-between'>
+                            <button className='text-[#51D1EE] font-bold text-xs border py-2 px-9 rounded-lg border-[#51D1EE]'>VIEW PROJECT</button>
+                            <div class="flex -space-x-1 overflow-hidden">
+                            <img class="inline-block h-5 w-5 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
+                            <img class="inline-block h-5 w-5 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
+                            <img class="inline-block h-5 w-5 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80" alt=""/>
+                            <img class="inline-block h-5 w-5 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
+                            </div>  
+                          </div>
                       </div>
 
 
