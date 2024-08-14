2c6d626f975fbd92ce8e587c7b7931f0e4012c90
JonathanDabre <109282867+JonathanDabre@users.noreply.github.com>
2024-08-14 18:30:24 +0530

    Update Credits.jsx

--- src/components/utlis/Credits.jsx
+++ src/components/utlis/Credits.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Credits = () => {
   return (
     <div className='flex flex-col px-5 md:px-0 my-6 justify-center items-center'>
-        <div className='text-[#67748E] font-semibold flex justify-center'> <div className='text-center'>  © 2024, Crafted with ❤️ by Simona & Jonathan</div></div>
+        <div className='text-[#67748E] font-semibold flex justify-center'> <div className='text-center'>  © 2024, Crafted with ❤️ by Simona</div></div>
     </div>
   )
 }
